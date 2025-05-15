a44a6f551201491d4a7b98a92d691a065fc3b28a
sidharth-n <sidharthnofficial@gmail.com>
2025-05-15 16:21:46 +0530

    update with analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/react"
+import App from "./App"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+      <Analytics />
+    </Router>
+  </React.StrictMode>
+)
